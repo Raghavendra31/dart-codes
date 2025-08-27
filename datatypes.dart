ece7ad3728ef5a410a56f5873f177ff0478e2d22
Raghavendra31 <r10756521@gmail.com>
2025-08-27 17:42:47 +0530

    done some homeworks

--- datatypes.dart
+++ datatypes.dart
@@ -4,3 +4,17 @@ void main(){
   String c = "Hello, Dart!";
   print("integer: $a, double: $b, string: $c");
 }
+
+// int 
+// string
+// double 
+// num // holds both int and double
+// bool
+// list<string> // array of strings/ same as list in python
+// list<int> // array of integers
+// map<string, int> // key-value pairs/same as dictionary in python
+// set<string> // unique values/same as set in python
+// var // automatically detects the datatype
+// dynamic // can hold any datatype and can be changed later
+// symbol // used in metaprogramming
+// null // represents absence of a value
---
+++ homeWork1.dart
@@ -0,0 +1,34 @@
+// void main() {
+//   Map<String, dynamic> marks = {
+//     "name":"raghav",
+//     "age": 20,
+//     "fvrt programming language": "Dart",
+//   };
+  
+//   print(marks); // 90
+// }
+
+
+// void main (){
+//   List<String> apps = ["instagram", "facebook", "whatsapp"];
+//   print(apps);
+// }
+
+
+void main(){
+  Map<String, dynamic> marks = {
+    "name":"raghav",
+    "age": 20,
+    "fvrt programming language": "Dart",
+  };
+  
+  print(marks);
+  List<String> apps = ["instagram", "facebook", "whatsapp"];
+  print(apps);
+  Map<String,double> marks1 = {
+    "maths": 90.5,
+    "science": 85.0,
+    "english": 88.5,
+  };
+print (marks1);
+}
