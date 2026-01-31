13695633a3a8cdc45f561393b73a012fff19f9a4
Raghavendra31 <r10756521@gmail.com>
2026-01-31 21:28:58 +0530

    transfering data from one to another page

--- lib/main.dart
+++ lib/main.dart
@@ -61,66 +61,66 @@
 import 'package:flutter/material.dart';
 
 
-void main(){
-  runApp(const MyApp());
-}
+// void main(){
+//   runApp(const MyApp());
+// }
 
-class MyApp extends StatelessWidget{
-  const MyApp({super.key});
+// class MyApp extends StatelessWidget{
+//   const MyApp({super.key});
 
-  @override 
-Widget build(BuildContext context){
-    return const MaterialApp(
-       debugShowCheckedModeBanner: false,
-       home: FirstScreen(),
-     );
-   }
-  } 
+//   @override 
+// Widget build(BuildContext context){
+//     return const MaterialApp(
+//        debugShowCheckedModeBanner: false,
+//        home: FirstScreen(),
+//      );
+//    }
+//   } 
 
-class FirstScreen extends StatelessWidget{
-  const FirstScreen({super.key});
+// class FirstScreen extends StatelessWidget{
+//   const FirstScreen({super.key});
 
-  @override
-  Widget build(BuildContext context){
-    return Scaffold(
-      appBar : AppBar(title: const Text("hello first")),
-      body: Center(
-         child: ElevatedButton(
-           onPressed: () {
-             Navigator.push(
-               context,
-               MaterialPageRoute(
-                 builder: (context) => const SecondScreen(),
-               ),
-             );
-           },
-           child: const Text("Go to Second Screen"),
-         ),
-       ),
-    );
-  }
-}
+//   @override
+//   Widget build(BuildContext context){
+//     return Scaffold(
+//       appBar : AppBar(title: const Text("hello first")),
+//       body: Center(
+//          child: ElevatedButton(
+//            onPressed: () {
+//              Navigator.push(
+//                context,
+//                MaterialPageRoute(
+//                  builder: (context) => const SecondScreen(),
+//                ),
+//              );
+//            },
+//            child: const Text("Go to Second Screen"),
+//          ),
+//        ),
+//     );
+//   }
+// }
 
-class SecondScreen extends StatelessWidget{
-  const SecondScreen({super.key});
+// class SecondScreen extends StatelessWidget{
+//   const SecondScreen({super.key});
 
   
-@override
-  Widget build(BuildContext context){
-    return Scaffold(
-    appBar : AppBar(title: const Text("second page")),
-    body : Center(
-      child : ElevatedButton(
-        onPressed: () {
-          Navigator.pop(context);
+// @override
+//   Widget build(BuildContext context){
+//     return Scaffold(
+//     appBar : AppBar(title: const Text("second page")),
+//     body : Center(
+//       child : ElevatedButton(
+//         onPressed: () {
+//           Navigator.pop(context);
 
-        },
-      child: const Text("go back"),
-      ),
-    ),
-    );
-  }
-}
+//         },
+//       child: const Text("go back"),
+//       ),
+//     ),
+//     );
+//   }
+// }
 //    @override
 //    Widget build(BuildContext context) {
 //      return Scaffold(
@@ -136,3 +136,150 @@ class SecondScreen extends StatelessWidget{
 //      );
 //    }
 //  }
+//
+//
+
+
+
+// void main(){
+//   runApp(const MyApp());
+// }
+
+// class MyApp extends StatelessWidget{
+  
+
+//   const MyApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context){
+//     return const MaterialApp(
+//       debugShowCheckedModeBanner : false,
+//       home : firstScreen()
+//     );
+    
+//   }
+// }
+
+// class firstScreen extends StatelessWidget {
+//   const firstScreen({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     String message = "Hello from First Screen!";
+
+//     return Scaffold(
+//       appBar: AppBar(title: const Text("First Screen")),
+//       body: Center(
+//         child: ElevatedButton(
+//           onPressed: () {
+//             Navigator.push(
+//               context,
+//               MaterialPageRoute(
+//                 builder: (context) => SecondScreen(text: message),
+//               ),
+//             );
+//           },
+//           child: const Text("Go to Second Screen"),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+
+
+// class SecondScreen extends StatelessWidget {
+
+//   final String text;
+
+//   const SecondScreen({super.key, required this.text});
+
+//   @override
+//   Widget build(BuildContext context){
+//     return Scaffold(
+//       appBar: AppBar(title : const Text("second page")),
+//       body : Center(
+//         child: Text(
+
+//         text,
+//         style: const TextStyle(fontSize: 20)
+//         )
+//       )
+//     );
+//   }
+
+// }
+
+
+
+
+void main(){
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget{
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: firstscreen(),
+      );
+  }
+}
+
+class firstscreen extends StatelessWidget{
+  
+
+  const firstscreen({super.key});
+
+  @override
+  Widget build(BuildContext context){
+    String message = "hello from first";
+    return Scaffold(
+     appBar: AppBar(title: const Text("first screen"),
+     ),
+      body: Center(
+        child : ElevatedButton(
+          onPressed:() {
+
+            Navigator.push(
+              context,
+               MaterialPageRoute(
+                 builder: (context) => SecondScreen(text: message),
+
+            ),
+            );
+          },
+        child : const Text("Go to Second Screen"),
+        ),
+      ),
+    );
+  }
+
+}
+
+class SecondScreen extends StatelessWidget{
+
+  final String text;
+
+  const SecondScreen({super.key, required this.text});
+
+  @override
+  Widget build(BuildContext context){
+    return Scaffold(
+      appBar: AppBar(title: const Text("hello second")),
+
+      body: Center(
+        child: Text(
+          text,
+          style : const TextStyle(fontSize:20),
+
+        ),
+        ),
+
+    );
+  }
+
+}
